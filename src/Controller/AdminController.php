d3cdef0d06e68507ce4b32c83b7247d0c5c4b7ce
Yannis699 <yannis.cristini@gmail.com>
2022-06-03 14:54:04 +0200

    ajout de l'ebook

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -6,16 +6,18 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
 #[Route(path:'/admin', name:'admin_')]
 class AdminController extends AbstractController
 {
-    #[Route(path:"/", name:"login")]
-    public function login(): Response
-    {
-        return $this->render('admin/login.html.twig');
-    }
 
-    #[Route(path:"/index", name: "index")]
+#[Route(path:"/", name:"login")]
+public function login(): Response
+{
+return $this->render('admin/login.html.twig');
+}
+
+#[Route(path:"/index", name: "index")]
     public function list(): Response
     {
         return $this->render(('admin/dashboard.html.twig'));
---
+++ src/Controller/EbookController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class EbookController extends AbstractController
+{
+    #[Route('/getebook', name: 'app_ebook')]
+    public function index(): Response
+    {
+        return $this->render('ebook/index.html.twig', [
+            'controller_name' => 'EbookController',
+        ]);
+    }
+}
