b9274d75673856db359ae6ca98213df8217a5b14
Yannis699 <yannis.cristini@gmail.com>
2022-06-03 14:58:04 +0200

    ajout de la route ebook

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,22 +2,20 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-
-#[Route(path:'/admin', name:'admin_')]
+#[Route(path: '/admin', name: 'admin_')]
 class AdminController extends AbstractController
 {
+    #[Route(path: "/", name: "login")]
+    public function login(): Response
+    {
+        return $this->render('admin/login.html.twig');
+    }
 
-#[Route(path:"/", name:"login")]
-public function login(): Response
-{
-return $this->render('admin/login.html.twig');
-}
-
-#[Route(path:"/index", name: "index")]
+    #[Route(path: "/index", name: "index")]
     public function list(): Response
     {
         return $this->render(('admin/dashboard.html.twig'));
