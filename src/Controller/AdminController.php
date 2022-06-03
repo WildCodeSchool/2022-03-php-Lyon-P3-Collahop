0033c6b5eedded26cba378223153ed0dcf736460
Yannis699 <yannis.cristini@gmail.com>
2022-06-03 13:38:24 +0200

    Ajout du préfixe de la route admin

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -6,15 +6,16 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route(path:'/admin', name:'admin_')]
 class AdminController extends AbstractController
 {
-    #[Route(path:"admin", name:"admin")]
+    #[Route(path:"/", name:"login")]
     public function login(): Response
     {
         return $this->render('admin/login.html.twig');
     }
 
-    #[Route(path:"admin/index", name: "admin_index")]
+    #[Route(path:"/index", name: "index")]
     public function list(): Response
     {
         return $this->render(('admin/dashboard.html.twig'));
