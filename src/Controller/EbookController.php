399452abfa93aeaad136fae05ea2f23f9f7d39b3
jugets <justine.gets@posteo.net>
2022-06-08 09:30:53 +0200

    Creation of ebook form

--- src/Controller/EbookController.php
+++ src/Controller/EbookController.php
@@ -2,17 +2,33 @@
 
 namespace App\Controller;
 
+use App\Entity\Contact;
+use App\Form\EbookType;
+use App\Repository\ContactRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class EbookController extends AbstractController
 {
-    #[Route('/get_ebook', name: 'app_ebook')]
-    public function index(): Response
+    #[Route('/get_ebook', name: 'ebook_form', methods: ['GET', 'POST'])]
+    public function new(Request $request, ContactRepository $contactRepository): Response
     {
-        return $this->render('ebook/index.html.twig', [
-            'controller_name' => 'EbookController',
+        $contact = new Contact();
+        $form = $this->createForm(EbookType::class, $contact);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $contactRepository->add($contact, true);
+            
+            return $this->redirectToRoute('/');
+        }
+
+        return $this->renderForm('/ebook/index.html.twig', [
+            'form' => $form,
+            'contact' => $contact,
         ]);
     }
+
 }
