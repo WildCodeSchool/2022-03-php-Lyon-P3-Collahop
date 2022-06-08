9898d5556352ccf5955710cbb6fb48eb63021891
HOURDOU DAVID <davidhourdou@gmail.com>
2022-06-08 17:08:27 +0200

    homepage part2 integratin

--- assets/app.js
+++ assets/app.js
@@ -7,6 +7,9 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.scss';
+import './styles/homepage.scss';
+import './styles/mixins.scss';
+
 require('bootstrap');
 
 // start the Stimulus application
