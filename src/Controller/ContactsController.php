451aa7d2d19052da7e0811e2a74798741ccacceb
Yannis699 <yannis.cristini@gmail.com>
2022-06-12 14:10:34 +0200

    Ajout du visuel du formulaire

--- src/Controller/ContactsController.php
+++ src/Controller/ContactsController.php
@@ -2,17 +2,26 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Contact;
+use App\Form\ContactsType;
+use App\Repository\ContactRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ContactsController extends AbstractController
 {
     #[Route('/contacts', name: 'app_contacts')]
-    public function index(): Response
+    public function new(Request $request, ContactRepository $contactRepository): Response
     {
-        return $this->render('contacts/index.html.twig', [
-            'controller_name' => 'ContactsController',
+        $contacts = new Contact();
+
+        $form = $this->createForm(ContactsType::class, $contacts);
+
+        return $this->renderForm('contacts/index.html.twig', [
+            'form' => $form,
+            'contacts' => $contacts
         ]);
     }
 }
