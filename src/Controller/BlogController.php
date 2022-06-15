913d3646b772371604d042e4203a87f6ff7c0432
Gautier Fondevila <fondevila.gautier@gmail.com>
2022-06-15 17:39:34 +0200

    creation of blog page. There is some small things to fix (color of the blue in background, and opacity of the background image)

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ArticlesRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BlogController extends AbstractController
+{
+    #[Route('/blog', name: 'app_blog')]
+    public function index(ArticlesRepository $articlesRepository): Response
+    {
+        $articles = $articlesRepository->findAll();
+        return $this->render('blog/index.html.twig', [
+            'controller_name' => 'BlogController',
+            'articles' => $articles
+        ]);
+    }
+}
---
+++ src/Entity/Articles.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ArticlesRepository;
+use DateTime;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ArticlesRepository::class)]
+class Articles
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private int $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private string $title;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private string $image;
+
+    #[ORM\Column(type: 'text')]
+    private string $article_content;
+
+    #[ORM\Column(type: 'text')]
+    private string $article_sumary;
+
+    #[ORM\Column(type: 'date')]
+    private mixed $date;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    public function getArticleContent(): ?string
+    {
+        return $this->article_content;
+    }
+
+    public function setArticleContent(string $article_content): self
+    {
+        $this->article_content = $article_content;
+
+        return $this;
+    }
+
+    public function getArticleSumary(): ?string
+    {
+        return $this->article_sumary;
+    }
+
+    public function setArticleSumary(string $article_sumary): self
+    {
+        $this->article_sumary = $article_sumary;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+}
