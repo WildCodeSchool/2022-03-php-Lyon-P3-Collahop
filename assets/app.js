892b051795c24e816822a8b292aea8b608b7cbbf
Gautier Fondevila <fondevila.gautier@gmail.com>
2022-06-16 14:26:14 +0200

    fix conflicts

--- assets/app.js
+++ assets/app.js
@@ -1,16 +1,5 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
-
-
 import './styles/app.scss';
-import './styles/homepage.scss';
-import './styles/mixins.scss';
-import './styles/contacts.scss';
-import "./styles/navbar.scss";
+
 require("bootstrap");
 
 var burgerMenu = document.getElementById("burger-menu");
