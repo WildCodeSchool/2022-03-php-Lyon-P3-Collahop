d9000880a273982296381a4f88d9ab466100eaf7
Yannis699 <yannis.cristini@gmail.com>
2022-06-20 10:12:23 +0200

    Ajout fonction window.onload

--- assets/app.js
+++ assets/app.js
@@ -4,6 +4,30 @@ require("bootstrap");
 require('@fortawesome/fontawesome-free/css/all.min.css');
 require('@fortawesome/fontawesome-free/js/all.js');
 
+/*====== Sidebar admin ======*/
+
+window.onload = () => {
+
+    let aside = document.querySelector('.sidebar_admin');
+    let icon = aside.querySelector('.menu_icon');
+    let li = aside.querySelectorAll('.side_item');
+    
+    icon.onclick = () => {
+        aside.classList.toggle('expand');
+    }
+    
+    for(let i of li) {
+        i.onclick = activeLi;
+    }
+    
+    function activeLi() {
+        const list = Array.from(li);
+        list.forEach(e=>e.classList.remove('active'));
+        this.classList.add('active');
+    }
+}   
+
+
 var burgerMenu = document.getElementById("burger-menu");
 
 var overlay = document.getElementById("mobile_menu");
@@ -17,6 +41,7 @@ burgerMenu.addEventListener("click", function () {
         document.documentElement.style.overflow = "hidden";
     }
 });
+
 
 
 
