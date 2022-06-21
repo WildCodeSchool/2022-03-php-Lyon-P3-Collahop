4161cf9f0000c265049e2f4a1435d8230bc9ebc5
HOURDOU DAVID <davidhourdou@gmail.com>
2022-06-21 15:36:58 +0200

    our services page

--- src/Controller/OurServicesController.php
+++ src/Controller/OurServicesController.php
@@ -11,8 +11,6 @@ class OurServicesController extends AbstractController
     #[Route('/our/services', name: 'app_our_services')]
     public function index(): Response
     {
-        return $this->render('our_services/index.html.twig', [
-            'controller_name' => 'OurServicesController',
-        ]);
+        return $this->render('our_services/index.html.twig');
     }
 }
