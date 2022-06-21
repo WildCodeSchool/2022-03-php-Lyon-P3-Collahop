850df012f56243aa63f7258e92b259e8bb631dd8
Gautier Fondevila <fondevila.gautier@gmail.com>
2022-06-21 19:49:03 +0200

    remove some comment and dead code (because I know that you will kick me off.. again ...)

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -28,7 +28,6 @@ class LoginController extends AbstractController
     #[Route('/logout', name: 'logout', methods: ['GET'])]
     public function logout(): void
     {
-        // controller can be blank: it will never be called!
         throw new Exception('Don\'t forget to activate logout in security.yaml');
     }
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -90,7 +90,5 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      */
     public function eraseCredentials(): void
     {
-        // If you store any temporary, sensitive data on the user, clear it here
-        // $this->plainPassword = null;
     }
 }
