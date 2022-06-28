1d34f970dd130bc7c9c82259a9b85dc835959548
Gautier Fondevila <fondevila.gautier@gmail.com>
2022-06-28 09:55:55 +0200

    fix 'errors' with the burger menu

--- assets/app.js
+++ assets/app.js
@@ -28,20 +28,20 @@ window.onload = () => {
 }   
 
 
-var burgerMenu = document.getElementById("burger-menu");
+/*====== Burger menu mobile ======*/
 
+const hamburgerMenu = document.getElementById('hamburgerMenu');
+var burgerMenu = document.getElementById("burger-menu");
 var overlay = document.getElementById("mobile_menu");
 
-burgerMenu.addEventListener("click", function () {
-    this.classList.toggle("close");
-    overlay.classList.toggle("overlay");
-    if (document.documentElement.style.overflow === "hidden") {
-        document.documentElement.style.overflow = "initial";
-    } else {
-        document.documentElement.style.overflow = "hidden";
-    }
-});
-
-
-
-
+if (burgerMenu){
+    burgerMenu.addEventListener("click", function () {
+        this.classList.toggle("close");
+        overlay.classList.toggle("overlay");
+        if (document.documentElement.style.overflow === "hidden") {
+            document.documentElement.style.overflow = "initial";
+        } else {
+            document.documentElement.style.overflow = "hidden";
+        }
+    })
+}
